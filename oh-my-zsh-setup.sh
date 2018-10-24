a9816b27a262c99d22f78c57c677c568a303db08
ivanCanaveral <canaveral.ivan@gmail.com>
2018-10-24 23:55:50 +0200

    symbolic link to zshrc created in setup

--- oh-my-zsh-setup.sh
+++ oh-my-zsh-setup.sh
@@ -4,12 +4,14 @@ sudo apt-get install curl
 chsh -s $(which zsh)
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
+ln -s ~/.oh-my-sh/.oh-my-zsh/zshrc ~/.zshrc
+
 git clone https://github.com/zsh-users/zsh-autosuggestions.git ~/.oh-my-zsh/plugins/zsh-autosuggestions
 sudo git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/zsh-syntax-highlighting
 git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
 
-exec bash #change to bash
-sudo apt-get install dconf-cli
-wget -O gogh https://git.io/vQgMr && chmod +x gogh && ./gogh && rm gogh
+#exec bash #change to bash
+#sudo apt-get install dconf-cli
+#wget -O gogh https://git.io/vQgMr && chmod +x gogh && ./gogh && rm gogh
 # the (43) Flat theme is cool
-exec zsh
+#exec zsh
